c3ffa54041abf04ea11d24998109ea4379283c1f
Kirill Nechaev <kirineech@gmail.com>
2026-04-28 16:34:27 +0300

    project setup

---
+++ src/shared/types/task.ts
@@ -0,0 +1,19 @@
+export type TaskStatus = 'todo' | 'inProgress' | 'done'
+export type TaskPriority = 'low' | 'medium' | 'high'
+
+export interface Task {
+  id: string
+  title: string
+  description?: string
+  status: TaskStatus
+  priority: TaskPriority
+  deadline: string
+  tags: string[]
+  createdAt: string
+  updatedAt: string
+}
+
+export interface Tag {
+  id: string
+  name: string
+}
